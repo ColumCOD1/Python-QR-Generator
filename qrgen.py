8fff05dbdb775f9f8de96c55434e52980ab20c6d
Pablo A. Garcia <zer0@Pablos-MacBook-Pro.local>
2022-10-17 15:27:45 +0900

    QR Generator code

---
+++ qrgen.py
@@ -0,0 +1,20 @@
+#install the llibraries needed
+#create the functions that collects the texts and converts it to a QR code
+# save the qr code as an image 
+#from asyncio import constants
+import qrcode 
+
+def generate_qrcode(text):
+
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black,", back_color="white")
+    img.save("qrimg.png")
+
+generate_qrcode("")
